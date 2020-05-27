ac711ed71836463a7ae20f4e8a4fa0e26c41335b
cliffckerr <questionably@gmail.com>
2020-05-26 21:10:43 -0700

    added data files

---
+++ us_states/data/README.md
@@ -0,0 +1,3 @@
+# State data
+
+Data retrieved from the Covasim data scrapers on 2020-05-26. Data from Covid Tracking Project.
--- us_states/load_data.py
+++ us_states/load_data.py
@@ -4,15 +4,34 @@ import pandas  as pd; pd
 import sciris  as sc; sc
 import covasim as cv; cv
 
-states = ['IL', 'MA', 'MI', 'NJ', 'NY', 'PA']
+
+#%% Initialize
+
+mapping = {
+    'IL':'Illinois',
+    'MA':'Massachusetts',
+    'MI':'Michigan',
+    'NJ':'New Jersey',
+    'NY':'New York',
+    'PA':'Pennsylvania',
+}
 folder = 'data'
 
-raw = sc.objdict()
+states = list(mapping.keys())
+
+
+#%% Get the raw data
+data = sc.objdict()
 for state in states:
     filename = f'{folder}/{state}.csv'
-    raw[state] = pd.read_csv(filename)
+    data[state] = pd.read_csv(filename)
 
 
+# From https://github.com/covid-modeling/covasim-connector/blob/master/runsim.py
+raw_pop_sizes = pd.read_csv("http://www2.census.gov/programs-surveys/popest/datasets/2010-2019/national/totals/nst-est2019-alldata.csv")
 
+popsizes = sc.objdict()
+for state in states:
+    popsizes[state] = int(raw_pop_sizes[raw_pop_sizes.NAME == mapping[state]].POPESTIMATE2019)
 
 print('Done.')
