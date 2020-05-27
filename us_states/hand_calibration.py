76e213e0e6512e5a1deb75c4c8364d884911f3bb
cliffckerr <questionably@gmail.com>
2020-05-26 22:33:38 -0700

    reasonably good quick fit

--- us_states/hand_calibration.py
+++ us_states/hand_calibration.py
@@ -11,7 +11,7 @@ pars = dict(
     pop_size = pop_size,
     pop_scale = data.popsize/pop_size,
     pop_infected = 1000,
-    beta = 0.018,
+    beta = 0.020,
     start_day = '2020-02-01',
     end_day   = '2020-06-01',
     rescale = True,
@@ -19,7 +19,14 @@ pars = dict(
 
 
 sim = cv.Sim(pars, datafile=data.epi)
+
+interventions = [
+    cv.change_beta(days=45, changes=0.33),
+    cv.test_num(daily_tests=sim.data['new_tests'].dropna(), symp_test=100),
+    ]
+
+sim.update_pars(interventions=interventions)
 sim.run()
-sim.plot(to_plot='overview', scatter_args=dict(alpha=0.1))
+sim.plot(to_plot='overview', scatter_args=dict(alpha=0.1), fig_args=dict(figsize=(30,20)))
 
 print('Done.')
