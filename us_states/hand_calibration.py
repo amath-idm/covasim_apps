d7461792a8eb7e48d98ba598d7f27a64209f70d1
cliffckerr <questionably@gmail.com>
2020-05-26 22:19:46 -0700

    basic hand calibration

--- us_states/hand_calibration.py
+++ us_states/hand_calibration.py
@@ -3,10 +3,23 @@ import load_data as ld
 
 state = 'NY'
 
-data = ld.load_data()
+all_data = ld.load_data()
+data = all_data[state]
 
-sim = cv.Sim(datafile=data[state].epi)
+pop_size = 100e3
+pars = dict(
+    pop_size = pop_size,
+    pop_scale = data.popsize/pop_size,
+    pop_infected = 1000,
+    beta = 0.018,
+    start_day = '2020-02-01',
+    end_day   = '2020-06-01',
+    rescale = True,
+    )
+
+
+sim = cv.Sim(pars, datafile=data.epi)
 sim.run()
-sim.plot(to_plot='overview')
+sim.plot(to_plot='overview', scatter_args=dict(alpha=0.1))
 
 print('Done.')
