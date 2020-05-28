9ec91605e3670faf7a24fd22b9002dc53fd11b84
cliffckerr <questionably@gmail.com>
2020-05-27 18:33:03 -0700

    fix michigan data

--- us_states/auto_calibration.py
+++ us_states/auto_calibration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pylab as pl
 import sciris as sc
 import covasim as cv
 import parestlib as pst
@@ -13,9 +14,9 @@ vb.plot    = 0
 vb.verbose = 0
 
 # Define and load the data
-state = 'NJ'
+state    = 'MI'  # Choose the state here!
 all_data = ld.load_data()
-data = all_data[state]
+data     = all_data[state]
 
 cv.check_version('1.4.0', die=True)
 
@@ -163,24 +164,36 @@ def calibrate():
 
 if __name__ == '__main__':
 
+    do_save = True
+
+    to_plot = ['cum_infections', 'new_infections', 'cum_tests', 'new_tests', 'cum_diagnoses', 'new_diagnoses', 'cum_deaths', 'new_deaths']
+
     # # Plot initial
+    print('Running initial...')
     pars, pkeys = get_bounds() # Get parameter guesses
-    # sim = create_sim(pars.best)
-    # sim.run()
-    # sim.plot(to_plot='overview')
-    # objective(pars.best)
+    sim = create_sim(pars.best)
+    sim.run()
+    sim.plot(to_plot=to_plot)
+    pl.gcf().axes[0].set_title('Initial parameter values')
+    objective(pars.best)
+    pl.pause(1.0) # Ensure it has time to render
 
     # Calibrate
+    print('Starting calibration for {state}...')
     T = sc.tic()
-    output = calibrate()
+    pars_calib = calibrate()
     sc.toc(T)
 
     # Plot result
-    x = [output[k] for k in pkeys]
+    print('Plotting result...')
+    x = [pars_calib[k] for k in pkeys]
     sim = create_sim(x)
     sim.run()
-    sim.plot(to_plot=['cum_infections', 'new_infections', 'cum_diagnoses', 'new_diagnoses', 'cum_deaths', 'new_deaths'], n_cols=2)
-    # sim.plot(to_plot='overview')
+    sim.plot(to_plot=to_plot)
+    pl.gcf().axes[0].set_title('Calibrated parameter values')
+
+    if do_save:
+        sc.savejson(f'calibrated_parameters_{state}.json', pars_calib)
 
 
 print('Done.')
